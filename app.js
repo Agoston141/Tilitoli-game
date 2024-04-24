e07abd2a250ffddd089f58d900694dba77e55b14
Agoston141 <moplnar.agoston2006@gmail.com>
2024-04-24 13:18:00 +0200

    gombok

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const container = document.querySelector(".container")
+const container =document.querySelector(".container")
 for (let i = 1; i < 16; i++) {
     let rn=Math.floor(Math.random()*15)
         container.innerHTML+=`<div class="box" id="${rn}">${rn}</div>`
   } 
+
+const boxes = document.querySelectorAll(".box")
+boxes.forEach
