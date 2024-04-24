6ad33865a3edb0add9d91acec76ea12a34ef7e03
Agoston141 <128032262+Agoston141@users.noreply.github.com>
2024-04-24 14:09:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const container =document.querySelector(".container")
-for (let i = 1; i < 16; i++) {
-    let rn=Math.floor(Math.random()*15)
-        container.innerHTML+=`<div class="box" id="${rn}">${rn}</div>`
-  } 
-
 const boxes = document.querySelectorAll(".box")
+for (let i = 1; i < 16; i++) {
+    container.innerHTML+=`<div class="box" id="${i}">${i}</div>`
+
+}
+function shuffleContainer() {
+    const container =document.querySelector(".container")
+    const boxes = document.querySelectorAll(".box")
+    const boxArray = Array.prototype.slice.call(boxes);
+    boxArray.sort(() => Math.random() - 0.5);
+    container.innerHTML = "";
+    boxArray.forEach(box => container.appendChild(box));
+}
+
+window.onload=()=>{
+    shuffleContainer()
+}
 const upButton=document.querySelector(".up")
 const downButton=document.querySelector(".down")
 const leftButton=document.querySelector(".left")
