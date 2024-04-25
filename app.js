5d461867fe2a1aadc08614c9169e517c2dbe4e5a
Agoston141 <molnar.agoston2006@gmail.com>
2024-04-25 21:11:08 +0200

    a

--- app.js
+++ app.js
@@ -1,47 +1,34 @@
-const NormalBoxes =document.querySelector(".NormalBoxes")
-const boxes = document.querySelectorAll(".box")
+const container =document.querySelector(".container")
+const boxes =document.querySelectorAll(".box, .boxEmpty")
 for (let i = 1; i < 16; i++) {
-    NormalBoxes.innerHTML+=`<div class="box" id="${i}">${i}</div>`
+    container.innerHTML+=`<div class="box" id="${i}">${i}</div>`
 
 }
 function shuffleContainer() {
-    const NormalBoxes =document.querySelector(".NormalBoxes")
-    const boxes = document.querySelectorAll(".box")
-    const boxArray = Array.prototype.slice.call(boxes);
-    boxArray.sort(() => Math.random() - 0.5);
-    NormalBoxes.innerHTML = "";
-    boxArray.forEach(box => NormalBoxes.appendChild(box));
+    const boxes = document.querySelectorAll(".box");
+    const contents = Array.prototype.map.call(boxes, box => box.textContent);
+    contents.sort(() => Math.random() - 0.5);
+    boxes.forEach((box, index) => box.textContent = contents[index]);
 }
 
 window.onload=()=>{
     shuffleContainer()
 }
-const upButton=document.querySelector(".up")
-const downButton=document.querySelector(".down")
-const leftButton=document.querySelector(".left")
-const rightButton=document.querySelector(".right")
 
-function boxclick(event){
-    const clickedBox = event.target;
-    let clickedId= event.target.id
-    const emptyBox=document.getElementById("_")
-    
-
-    const clickedBoxParent = clickedBox.parentNode;
-    const emptyBoxParent = emptyBox.parentNode;
-    clickedBoxParent.removeChild(clickedBox)
-    emptyBoxParent.removeChildChild(emptyBox)
-    emptyBoxParent.appendChild(clickedBox);
-    clickedBoxParent.appendChild(emptyBox);
-
-    clickedBox.id = "_"
-    emptyBox.id = clickedId
-    console.log(clickedBox)
-    console.log(clickedId)
-}
-
-
-boxes.forEach(box=>{
-    box.addEventListener("click",boxclick)
+const emptyBox = document.querySelector("#_")
+const up=document.querySelector(".up")
+up.addEventListener("click",(event)=>{
+    console.log(event.target.id)
 })
+
+boxes.forEach(box => {
+    box.addEventListener("click", (event) => {
+        const clickedBox = event.target;
+        const tempContent = clickedBox.textContent;
+        clickedBox.textContent = emptyBox.textContent;
+        emptyBox.textContent = tempContent;
+        console.log(event.target.id)
+
+    });
+});
 
