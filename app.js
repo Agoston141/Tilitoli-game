ed7263880761140d0821f4062fb69ce868e03cd4
Agoston141 <128032262+Agoston141@users.noreply.github.com>
2024-04-26 13:33:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const container =document.querySelector(".container")
-const boxes =document.querySelectorAll(".box, .boxEmpty")
+const shuffe= document.querySelector(".shuffle")
 for (let i = 1; i < 16; i++) {
     container.innerHTML+=`<div class="box" id="${i}">${i}</div>`
 
 }
+const boxes1 =document.querySelectorAll(".box")
 function shuffleContainer() {
     const boxes = document.querySelectorAll(".box");
     const contents = Array.prototype.map.call(boxes, box => box.textContent);
@@ -15,20 +16,23 @@ window.onload=()=>{
     shuffleContainer()
 }
 
-const emptyBox = document.querySelector("#_")
-const up=document.querySelector(".up")
-up.addEventListener("click",(event)=>{
-    console.log(event.target.id)
+shuffe.addEventListener("click",()=>{
+    shuffleContainer()
+    emptyBox.textContent=""
 })
 
-boxes.forEach(box => {
+const emptyBox = document.querySelector(".boxEmpty")
+
+
+boxes1.forEach(box => {
+    
     box.addEventListener("click", (event) => {
-        const clickedBox = event.target;
-        const tempContent = clickedBox.textContent;
-        clickedBox.textContent = emptyBox.textContent;
-        emptyBox.textContent = tempContent;
-        console.log(event.target.id)
+        const clickedBox = event.target.textContent;
+        box.textContent = ""
+        emptyBox.textContent = clickedBox;
+
 
     });
 });
+
 
