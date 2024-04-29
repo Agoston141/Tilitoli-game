758dc8bcb0fcd41c143c4b257b58a57b80999882
Agoston141 <molnar.agoston2006@gmail.com>
2024-04-29 14:20:27 +0200

    a

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ for (let i = 1; i < 16; i++) {
     container.innerHTML+=`<div class="box" id="${i}">${i}</div>`
 
 }
-const boxes1 =document.querySelectorAll(".box")
+const boxes1 =document.querySelectorAll(".box,.boxEmpty")
 function shuffleContainer() {
     const boxes = document.querySelectorAll(".box");
     const contents = Array.prototype.map.call(boxes, box => box.textContent);
@@ -35,10 +35,12 @@ boxes1.forEach(box => {
         
     });
 });
+const emptyBox1 = document.querySelector(".boxEmpty")
+const boxes2 =document.querySelectorAll(".box")
 
 let answer=false
 const checkButton = document.querySelector(".check")
-boxes1.forEach(box =>{
+boxes2.forEach(box =>{
     checkButton.addEventListener("click",()=>{
         if (box.id === box.textContent && emptyBox1.id === emptyBox1.textContent){
             answer=true 
